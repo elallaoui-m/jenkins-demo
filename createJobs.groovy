0808a64ed5a789888810087e8e63defba1d98e9b
ELALLAOUI <elallaoui.elmehdi@gmail.com>
2021-02-05 23:14:18 +0100

    CMI

--- createJobs.groovy
+++ createJobs.groovy
@@ -1,19 +1,38 @@
-pipelineJob('pipelineJob') {
-    definition {
-        cps {
-            script(readFileFromWorkspace('pipelineJob.groovy'))
-            sandbox()
-        }
-    }
-}
+// pipelineJob('pipelineJob') {
+//     definition {
+//         cps {
+//             script(readFileFromWorkspace('pipelineJob.groovy'))
+//             sandbox()
+//         }
+//     }
+// }
 
-pipelineJob('theme-park-job') {
+// pipelineJob('theme-park-job') {
+//     definition {
+//         cpsScm {
+//             scm {
+//                 git {
+//                     remote {
+//                        github('elallaoui-m/spring-demo')
+//                        credentials('tmp-creds') 
+//                     }
+//                     branch 'master'
+//                 }
+//             }
+//             triggers {
+//                 githubPush()
+//             }
+//         }
+//     }
+// }
+
+pipelineJob('cmi-service') {
     definition {
         cpsScm {
             scm {
                 git {
                     remote {
-                       github('elallaoui-m/spring-demo')
+                       github('ENSAPAY/CMI')
                        credentials('tmp-creds') 
                     }
                     branch 'master'
